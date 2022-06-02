746de920d7f57b350d6ec736fda0e965cc762443
Debargha Guha Neogi <62098146+debar2001@users.noreply.github.com>
2022-06-02 10:35:53 +0530

    Time: 60 ms (73.26%), Space: 14.4 MB (18.60%) - LeetHub

---
+++ 867-transpose-matrix/867-transpose-matrix.py
@@ -0,0 +1,5 @@
+class Solution(object):
+    def transpose(self, matrix):
+        result = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+        return result
+        
