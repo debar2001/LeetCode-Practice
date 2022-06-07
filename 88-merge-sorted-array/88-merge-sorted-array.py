070c4bc1dc7489ce1752a31478ad1a01c2b65b5d
Debargha Guha Neogi <62098146+debar2001@users.noreply.github.com>
2022-06-07 10:07:48 +0530

    Time: 42 ms (21.14%), Space: 13.3 MB (92.06%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -7,9 +7,35 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        if m<1:
-            nums1[:] = nums2[:n]
-        elif n<1:
-            nums1[:] = nums1[:m]
-        else:
-            nums1[:] = sorted(nums1[:m]+nums2[:n])
+        i = m-1
+        j = n-1
+        indexreq = m+n-1
+        
+        while i>=0 and j>=0:
+            if nums1[i] > nums2[j]:
+                nums1[indexreq] = nums1[i]
+                i -= 1
+                indexreq -= 1
+            elif nums2[j] >= nums1[i]:
+                nums1[indexreq] = nums2[j]
+                j -= 1
+                indexreq -= 1
+            else:
+                nums1[indexreq] = nums2[j]
+                nums1[indexreq-1] = nums1[j]
+                i -= 1
+                j -= 1
+                indexreq -= 2
+        
+        while j>=0:
+            nums1[indexreq] = nums2[j]
+            j -= 1
+            indexreq -= 1
+        
+        # Brute Force Approach
+        # if m<1:
+        #     nums1[:] = nums2[:n]
+        # elif n<1:
+        #     nums1[:] = nums1[:m]
+        # else:
+        #     nums1[:] = sorted(nums1[:m]+nums2[:n])
