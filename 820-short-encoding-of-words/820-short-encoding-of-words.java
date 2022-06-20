3cf994b10ce97081df65821e3a4876cd71e08be6
Debargha Guha Neogi <62098146+debar2001@users.noreply.github.com>
2022-06-20 15:38:25 +0530

    Time: 650 ms (7.98%), Space: 50.3 MB (76.69%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.java
+++ 820-short-encoding-of-words/820-short-encoding-of-words.java
@@ -1,11 +1,12 @@
-class Solution:
-    def minimumLengthEncoding(self, words: List[str]) -> int:
-        word_set = set(words)
-         
-        for word in words:
-            n = len(word)
-            for j in range(n-1):
-                if word[n-j-1:] in word_set and word[n-j-1:]!= word:
-                    word_set.remove(word[n-1-j:])
-        return sum([len(i)+1 for i in word_set]) #1 is added for #
-        
+class Solution {
+    public int minimumLengthEncoding(String[] words) {
+        Arrays.sort(words,(a,b)-> b.length()-a.length());
+        StringBuffer sb = new StringBuffer();
+        for(String word: words){
+            if(sb.indexOf(word+"#")==-1){
+                sb.append(word+"#");
+            }
+        }
+        return sb.length();
+    }
+}
