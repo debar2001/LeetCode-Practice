3e92ad13c9ca85042d72a7623c9e89e33b16c354
Debargha Guha Neogi <62098146+debar2001@users.noreply.github.com>
2022-06-20 15:37:27 +0530

    Time: 175 ms (81.86%), Space: 14.7 MB (66.05%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.py
+++ 820-short-encoding-of-words/820-short-encoding-of-words.py
@@ -1,24 +1,11 @@
-from collections import deque
 class Solution:
-    def minimumLengthEncoding(self, words):
-        trie = {"level":0} #level keeps track of length of the traversed word
+    def minimumLengthEncoding(self, words: List[str]) -> int:
+        word_set = set(words)
+         
         for word in words:
-            curr = trie
-            for j in word[::-1]:
-                if j not in curr:
-                    curr[j] = {}
-                    curr[j]["level"] = curr["level"]+1 
-                curr = curr[j]
-        
-        q = deque([trie])
-        s = 0
-        while q:
-            curr = q.popleft()
-            for i in curr:
-                if i!="level":
-                    if len(curr[i])==1:
-                        s+=curr[i]["level"] + 1 #Adding 1 for #
-                    else:
-                        q.append(curr[i])
-        return s
+            n = len(word)
+            for j in range(n-1):
+                if word[n-j-1:] in word_set and word[n-j-1:]!= word:
+                    word_set.remove(word[n-1-j:])
+        return sum([len(i)+1 for i in word_set]) #1 is added for #
         
