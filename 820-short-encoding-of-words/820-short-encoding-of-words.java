22e39dc81a5e1c8dfdef2413f541c17da4f820ae
Debargha Guha Neogi <62098146+debar2001@users.noreply.github.com>
2022-06-20 15:39:31 +0530

    Time: 29 ms (71.17%), Space: 59.2 MB (47.85%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.java
+++ 820-short-encoding-of-words/820-short-encoding-of-words.java
@@ -1,12 +1,40 @@
 class Solution {
-    public int minimumLengthEncoding(String[] words) {
-        Arrays.sort(words,(a,b)-> b.length()-a.length());
-        StringBuffer sb = new StringBuffer();
-        for(String word: words){
-            if(sb.indexOf(word+"#")==-1){
-                sb.append(word+"#");
-            }
-        }
-        return sb.length();
+class TrieNode{
+TrieNode []children;
+
+    TrieNode(){
+        children = new TrieNode[26];
     }
+}
+TrieNode root;
+public boolean helper(String word){
+    
+    boolean flag = true;
+    TrieNode curr = root;
+    
+    for(int i=word.length()-1;i>=0;i--){
+        char ch = word.charAt(i);
+        if(curr.children[ch-'a'] == null){
+            flag = false;
+            curr.children[ch - 'a'] = new TrieNode();
+        }
+        curr = curr.children[ch-'a'];
+    }
+    return flag;
+}
+public int minimumLengthEncoding(String[] words) {
+    
+    root = new TrieNode();
+    Arrays.sort(words,(a,b) ->{
+        return b.length() - a.length();
+    });
+    
+    int count = 0;
+    for(int i=0;i<words.length;i++){
+        if(!helper(words[i])){
+            count += words[i].length() + 1;
+        }
+    }
+    return count;
+}
 }
