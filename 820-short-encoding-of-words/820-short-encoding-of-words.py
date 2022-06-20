b45410da77cf585fd90a70db928834d2284d1f28
Debargha Guha Neogi <62098146+debar2001@users.noreply.github.com>
2022-06-20 15:36:36 +0530

    Time: 352 ms (38.10%), Space: 16.3 MB (52.38%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.py
+++ 820-short-encoding-of-words/820-short-encoding-of-words.py
@@ -1,23 +1,24 @@
+from collections import deque
 class Solution:
-    def minimumLengthEncoding(self, words: List[str]) -> int: 
-        l=[]
-        l2=[]
-        for i in range(len(words)): #sorting the words array in descending order of words[i]'s length and stored at l2 or you can use words.sort(key=len)
-            l.append(len(words[i]))
-        l3=l.copy()
-        l.sort()
-        l=l[::-1]
-        for i in range(len(words)):
-            new=l3.index(l[i])
-            l2.append(words[new])
-            l3[new]=0
-        print(l2)
-        s=""
-        s=s+l2[0]+"#"
-        for i in range(1,len(l2)):
-            st=l2[i]+"#" 
-            if st not in s:
-                s=s+st
-        print(s)
-        return len(s)
+    def minimumLengthEncoding(self, words):
+        trie = {"level":0} #level keeps track of length of the traversed word
+        for word in words:
+            curr = trie
+            for j in word[::-1]:
+                if j not in curr:
+                    curr[j] = {}
+                    curr[j]["level"] = curr["level"]+1 
+                curr = curr[j]
+        
+        q = deque([trie])
+        s = 0
+        while q:
+            curr = q.popleft()
+            for i in curr:
+                if i!="level":
+                    if len(curr[i])==1:
+                        s+=curr[i]["level"] + 1 #Adding 1 for #
+                    else:
+                        q.append(curr[i])
+        return s
         
