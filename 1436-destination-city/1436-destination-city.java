6dc5521ecfe5402927a8e4121245b6aea492751f
Debargha Guha Neogi <62098146+debar2001@users.noreply.github.com>
2022-08-02 20:16:49 +0530

    Time: 1 ms (100.00%), Space: 41.9 MB (93.88%) - LeetHub

--- 1436-destination-city/1436-destination-city.java
+++ 1436-destination-city/1436-destination-city.java
@@ -1,26 +1,36 @@
 class Solution {
     public String destCity(List<List<String>> paths) {
-        HashMap<String, Integer> map = new HashMap<>();
-        for(int i=0; i<paths.size(); i++){
-            for(int j=0; j<paths.get(i).size(); j++){
-               if(j==0){
-                  int count = map.containsKey(paths.get(i).get(j))? map.get(paths.get(i).get(j)):0;
-                  map.put(paths.get(i).get(j), count-1);
-               }
-                else{
-                    int count = map.containsKey(paths.get(i).get(j))? map.get(paths.get(i).get(j)):0;
-                    map.put(paths.get(i).get(j), count+1);
-                }
-            }
-        }
-        String ans = "";
-        for(String key: map.keySet()){
-            if(map.get(key)==1){
-                ans = key;
-                break;
-            }
-        }
+//         HashMap<String, Integer> map = new HashMap<>();
+//         for(int i=0; i<paths.size(); i++){
+//             for(int j=0; j<paths.get(i).size(); j++){
+//                if(j==0){
+//                   int count = map.containsKey(paths.get(i).get(j))? map.get(paths.get(i).get(j)):0;
+//                   map.put(paths.get(i).get(j), count-1);
+//                }
+//                 else{
+//                     int count = map.containsKey(paths.get(i).get(j))? map.get(paths.get(i).get(j)):0;
+//                     map.put(paths.get(i).get(j), count+1);
+//                 }
+//             }
+//         }
+//         String ans = "";
+//         for(String key: map.keySet()){
+//             if(map.get(key)==1){
+//                 ans = key;
+//                 break;
+//             }
+//         }
         
-        return ans;
+//         return ans;
+        
+        HashSet<String> myHashSet=new HashSet();
+        for(int i=0;i<paths.size();i++)
+              myHashSet.add(paths.get(i).get(0));
+        
+        for(int i=0;i<paths.size();i++) {
+            if(!myHashSet.contains(paths.get(i).get(1)))
+                return paths.get(i).get(1);
+        }
+        return null;
     }
 }
