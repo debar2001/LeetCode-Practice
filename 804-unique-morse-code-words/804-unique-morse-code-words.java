bf52bc720d9a74718946f5009b7be9fe439401b3
Debargha Guha Neogi <62098146+debar2001@users.noreply.github.com>
2022-08-17 10:49:11 +0530

    Time: 3 ms (80.13%), Space: 41.8 MB (73.89%) - LeetHub

---
+++ 804-unique-morse-code-words/804-unique-morse-code-words.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        HashMap<Character, String> map = new HashMap<>();
+        map.put('a', ".-");
+        map.put('b', "-...");
+        map.put('c', "-.-.");
+        map.put('d', "-..");
+        map.put('e', ".");
+        map.put('f', "..-.");
+        map.put('g', "--.");
+        map.put('h', "....");
+        map.put('i', "..");
+        map.put('j', ".---");
+        map.put('k', "-.-");
+        map.put('l', ".-..");
+        map.put('m', "--");
+        map.put('n', "-.");
+        map.put('o', "---");
+        map.put('p', ".--.");
+        map.put('q', "--.-");
+        map.put('r', ".-.");
+        map.put('s', "...");
+        map.put('t', "-");
+        map.put('u', "..-");
+        map.put('v', "...-");
+        map.put('w', ".--");
+        map.put('x', "-..-");
+        map.put('y', "-.--");
+        map.put('z', "--..");
+        
+        HashSet<String> set = new HashSet<>();
+        
+        for(int i=0; i<words.length; i++){
+            String ans = "";
+            char[] arr = words[i].toCharArray();
+            for(char ch:arr){
+                ans += map.get(ch);
+            }
+            set.add(ans);
+        }
+        
+        return set.size();
+    }
+}
