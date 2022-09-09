069b8ddc0663ccc6b9f435279f245310cfbed857
Debargha Guha Neogi <62098146+debar2001@users.noreply.github.com>
2022-09-09 11:08:52 +0530

    Time: 0 ms (100.00%), Space: 43 MB (32.75%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,33 +1,43 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m-1;
-        int j = n-1;
-        int indreq = m+n-1;
+//         int i = m-1;
+//         int j = n-1;
+//         int indreq = m+n-1;
         
-        while(i>=0 && j>=0){
-            if(nums1[i] > nums2[j]){
-                nums1[indreq] = nums1[i];
-                i--;
-                indreq--;
-            }
-            else if(nums2[j] > nums1[i]){
-                nums1[indreq] = nums2[j];
-                j--;
-                indreq--;
-            }
+//         while(i>=0 && j>=0){
+//             if(nums1[i] > nums2[j]){
+//                 nums1[indreq] = nums1[i];
+//                 i--;
+//                 indreq--;
+//             }
+//             else if(nums2[j] > nums1[i]){
+//                 nums1[indreq] = nums2[j];
+//                 j--;
+//                 indreq--;
+//             }
+//             else{
+//                 nums1[indreq] = nums1[i];
+//                 nums1[indreq-1] = nums2[j];
+//                 i--; 
+//                 j--;
+//                 indreq -= 2;
+//             }
+//         }
+        
+//         while(j>=0){
+//             nums1[indreq] = nums2[j];
+//             j--; 
+//             indreq--;
+//         }
+        
+        int p1 = m-1 , p2 = n-1 ,i = m+n-1;
+        while(p2 >=0 ){
+            if(p1 >=0 && nums1[p1] > nums2[p2]){
+                nums1[i--] = nums1[p1--];
+            } 
             else{
-                nums1[indreq] = nums1[i];
-                nums1[indreq-1] = nums2[j];
-                i--; 
-                j--;
-                indreq -= 2;
+                nums1[i--] = nums2[p2--];
             }
-        }
-        
-        while(j>=0){
-            nums1[indreq] = nums2[j];
-            j--; 
-            indreq--;
         }
     }
 }
